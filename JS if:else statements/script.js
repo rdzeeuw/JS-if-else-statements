0e42b3d742a51c78d300a25e3c719c19650e7773
rdzeeuw <77979140+rdzeeuw@users.noreply.github.com>
2021-04-08 11:19:29 +0200

    Add files via upload

---
+++ JS if:else statements/script.js
@@ -0,0 +1,26 @@
+//deel 1
+const age = 10;
+
+if (age >= 18) {
+    console.log("You may enter the bar");
+} else {
+    console.log("Go home to your mommy");
+}
+
+//deel 2
+const isFemale = true;
+
+if(isFemale == true) {
+    console.log("You may enter the bar");
+} else {
+    console.log("Sorry dude, better luck next time!");
+}
+
+//deel 3
+const driverStatus = 'bob';
+
+if(driverStatus == 'bob') {
+    console.log('Save travels!');
+} else {
+console.log("Can you call someone to pick you up?");
+}
